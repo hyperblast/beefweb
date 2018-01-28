38943752ecb389f622ca20eb0c2bd87dc54c0dd6
Hyperblast <hypercode@zoho.com>
2018-01-28 13:00:31 +0300

    add windows version of apply-patches.sh

---
+++ server/extlibs/apply-patches.cmd
@@ -0,0 +1,26 @@
+@if "%1" == "" goto :usage
+
+@set src_dir=%~dp0%1
+@set patches_dir=%src_dir%\patches
+@set files_dir=%src_dir%\files
+
+@if exist "%patches_dir%\." call :apply_patches
+@if exist "%files_dir%\." call :copy_files
+@goto :end
+
+:apply_patches
+@for %%f in (%patches_dir%\*.patch) do @(
+    @echo applying %%~nxf
+    @patch -p1 --binary < "%%f"
+)
+@goto :end
+
+:copy_files
+@xcopy /E /F /Y "%files_dir%\*" .
+@goto :end
+
+:usage
+@echo usage: %~nx0 target
+@echo.
+
+:end
