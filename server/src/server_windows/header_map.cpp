a0f6e7467a4e83a3864000c27099646cd0255513
Hyperblast <hypercode@zoho.com>
2018-02-17 19:08:06 +0300

    server_windows: response header mapping

--- server/src/server_windows/header_map.cpp
+++ server/src/server_windows/header_map.cpp
@@ -1,4 +1,8 @@
 #include "header_map.hpp"
+#include "../defines.hpp"
+
+#include <assert.h>
+#include <unordered_map>
 
 namespace msrv {
 namespace server_windows {
@@ -23,7 +27,7 @@ const char* const REQUEST_HEADER_MAP[] =
     "Content-Encoding",     // HttpHeaderContentEncoding = 13
     "Content-Language",     // HttpHeaderContentLanguage = 14
     "Content-Location",     // HttpHeaderContentLocation = 15
-    "Content-Md5",          // HttpHeaderContentMd5 = 16
+    "Content-MD5",          // HttpHeaderContentMd5 = 16
     "Content-Range",        // HttpHeaderContentRange = 17
     "Expires",              // HttpHeaderExpires = 18
     "Last-Modified",        // HttpHeaderLastModified = 19
@@ -48,6 +52,70 @@ const char* const REQUEST_HEADER_MAP[] =
     "Te",                   // HttpHeaderTe = 38
     "Translate",            // HttpHeaderTranslate = 39
     "User-Agent",           // HttpHeaderUserAgent = 40
+};
+
+class ResponseHeaderMap
+{
+public:
+    static const ResponseHeaderMap* instance()
+    {
+        static ResponseHeaderMap map;
+        return &map;
+    }
+
+    int getId(const std::string& key) const
+    {
+        auto it = map_.find(key);
+        return it != map_.end() ? it->second : -1;
+    }
+
+private:
+    ResponseHeaderMap()
+    {
+        map_.reserve(HttpHeaderResponseMaximum);
+
+        add("Cache-Control", HttpHeaderCacheControl);
+        add("Connection", HttpHeaderConnection);
+        add("Date", HttpHeaderDate);
+        add("Keep-Alive", HttpHeaderKeepAlive);
+        add("Pragma", HttpHeaderPragma);
+        add("Trailer", HttpHeaderTrailer);
+        add("Transfer-Encoding", HttpHeaderTransferEncoding);
+        add("Upgrade", HttpHeaderUpgrade);
+        add("Via", HttpHeaderVia);
+        add("Warning", HttpHeaderWarning);
+        add("Allow", HttpHeaderAllow);
+        add("Content-Length", HttpHeaderContentLength);
+        add("Content-Type", HttpHeaderContentType);
+        add("Content-Encoding", HttpHeaderContentEncoding);
+        add("Content-Language", HttpHeaderContentLanguage);
+        add("Content-Location", HttpHeaderContentLocation);
+        add("Content-MD5", HttpHeaderContentMd5);
+        add("Content-Range", HttpHeaderContentRange);
+        add("Expires", HttpHeaderExpires);
+        add("Last-Modified", HttpHeaderLastModified);
+        add("Accept-Ranges", HttpHeaderAcceptRanges);
+        add("Age", HttpHeaderAge);
+        add("ETag", HttpHeaderEtag);
+        add("Location", HttpHeaderLocation);
+        add("Proxy-Authenticate", HttpHeaderProxyAuthenticate);
+        add("Retry-After", HttpHeaderRetryAfter);
+        add("Server", HttpHeaderServer);
+        add("Set-Cookie", HttpHeaderSetCookie);
+        add("Header-Vary", HttpHeaderVary);
+        add("Www-Authenticate", HttpHeaderWwwAuthenticate);
+
+        assert(map_.size() == HttpHeaderResponseMaximum);
+    }
+
+    void add(const char* key, int value)
+    {
+        map_.emplace(key, static_cast<USHORT>(value));
+    }
+
+    std::unordered_map<std::string, USHORT> map_;
+
+    MSRV_NO_COPY_AND_ASSIGN(ResponseHeaderMap);
 };
 
 }
@@ -87,6 +155,31 @@ void mapResponseHeaders(
     HTTP_RESPONSE_HEADERS* to,
     std::vector<HTTP_UNKNOWN_HEADER>* toUnknown)
 {
+    auto map = ResponseHeaderMap::instance();
+
+    for (auto& pair : *from)
+    {
+        auto knownId = map->getId(pair.first);
+
+        if (knownId >= 0)
+        {
+            auto* header = &to->KnownHeaders[knownId];
+            header->pRawValue = pair.second.data();
+            header->RawValueLength = static_cast<USHORT>(pair.second.length());
+        }
+        else
+        {
+            HTTP_UNKNOWN_HEADER header;
+            header.pName = pair.first.data();
+            header.NameLength = static_cast<USHORT>(pair.first.length());
+            header.pRawValue = pair.second.data();
+            header.RawValueLength = static_cast<USHORT>(pair.second.length());
+            toUnknown->push_back(header);
+        }
+    }
+
+    to->pUnknownHeaders = toUnknown->empty() ? nullptr : toUnknown->data();
+    to->UnknownHeaderCount = static_cast<USHORT>(toUnknown->size());
 }
 
 }}
