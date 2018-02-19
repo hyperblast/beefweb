4a815eb8dfcb8d3ef6ecebe65452e11738709431
Hyperblast <hypercode@zoho.com>
2018-02-19 17:30:16 +0300

    get-patch.cmd and minimal appveyor.yml

---
+++ scripts/get-patch.cmd
@@ -0,0 +1,21 @@
+setlocal
+
+cd "%~dp0.."
+
+if exist tools\patch\. rmdir /s /q tools\patch
+mkdir tools\patch
+cd tools\patch
+
+if errorlevel 1 goto :end
+
+curl --silent --fail --show-error --location -o patch.zip ^
+    "https://hyperblast.org/files/patch/patch.zip"
+if errorlevel 1 goto :end
+
+7za x patch.zip
+if errorlevel 1 goto :end
+
+del patch.zip
+if errorlevel 1 goto :end
+
+:end
