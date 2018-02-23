d336d59de824e0cc05811742b6c93ef4a1d2c34c
Hyperblast <hypercode@zoho.com>
2018-02-23 15:24:33 +0300

    Hello world foobar2000 plugin

---
+++ server/src/plugin_foobar/plugin.cpp
@@ -0,0 +1,14 @@
+#include "plugin.hpp"
+#include "../project_info.hpp"
+
+DECLARE_COMPONENT_VERSION(
+    MSRV_PROJECT_ID,
+    MSRV_VERSION_STRING,
+    MSRV_PROJECT_DESC "\n\n" MSRV_LICENSE_TEXT);
+
+VALIDATE_COMPONENT_FILENAME("foo_beefweb.dll");
+
+namespace msrv {
+namespace plugin_foobar {
+
+}}
