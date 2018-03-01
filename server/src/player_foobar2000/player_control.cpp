8d95e97e7dde1cd1be883a60d30cf74b38525c8a
Hyperblast <hypercode@zoho.com>
2018-03-01 18:33:55 +0300

    implementing player api

--- server/src/player_foobar2000/player_control.cpp
+++ server/src/player_foobar2000/player_control.cpp
@@ -3,9 +3,30 @@
 namespace msrv {
 namespace player_foobar2000 {
 
+PlaybackState PlayerImpl::getPlaybackState()
+{
+    if (playbackControl_->is_paused())
+       return PlaybackState::PAUSED;
+    
+    if (playbackControl_->is_playing())
+        return PlaybackState::PLAYING;
+
+    return PlaybackState::STOPPED;
+}
+
+void PlayerImpl::queryVolume(VolumeInfo* volume)
+{
+    volume->db = playbackControl_->get_volume();
+    volume->dbMin = playback_control::volume_mute;
+    volume->isMuted = playbackControl_->is_muted();
+}
+
 PlayerStatePtr PlayerImpl::queryPlayerState(TrackQuery* activeItemQuery)
 {
-    return std::make_unique<PlayerState>();
+    auto state = std::make_unique<PlayerState>();
+    state->playbackState = getPlaybackState();
+    queryVolume(&state->volume);
+    return state;
 }
 
 void PlayerImpl::playCurrent()
@@ -19,30 +40,52 @@ void PlayerImpl::playItem(const PlaylistRef& playlist, int32_t itemIndex)
 
 void PlayerImpl::playRandom()
 {
+    playbackControl_->start(playback_control::track_command_rand);
 }
 
 void PlayerImpl::playNext()
 {
+    playbackControl_->next();
 }
 
 void PlayerImpl::playPrevious()
 {
+    playbackControl_->previous();
 }
 
 void PlayerImpl::stop()
 {
+    playbackControl_->stop();
 }
 
 void PlayerImpl::pause()
 {
+    playbackControl_->pause(true);
 }
 
 void PlayerImpl::togglePause()
 {
+    playbackControl_->toggle_pause();
 }
 
 void PlayerImpl::setMuted(Switch val)
 {
+    switch (val)
+    {
+    case Switch::FALSE:
+        if (playbackControl_->is_muted())
+            playbackControl_->volume_mute_toggle();
+        break;
+
+    case Switch::TRUE:
+        if (!playbackControl_->is_muted())
+            playbackControl_->volume_mute_toggle();
+        break;
+
+    case Switch::TOGGLE:
+        playbackControl_->volume_mute_toggle();
+        break;
+    }
 }
 
 void PlayerImpl::seekAbsolute(double offsetSeconds)
