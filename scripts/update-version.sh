88d1258543d8e872a17c0a6c2dfa9c600880429f
Hyperblast <hypercode@zoho.com>
2018-03-16 15:49:00 +0300

    add script to update version

---
+++ scripts/update-version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+major=$1
+minor=$2
+
+if [ -z "$major" ] || [ -z "$minor" ]; then
+    echo "usage: $(basename $0) major minor"
+    exit 1
+fi
+
+version=$major.$minor
+
+cd "$(dirname $0)"
+sed -i "s|pkg_version=.*|pkg_version=$version|" config.sh
+sed -i "s|<PackageVersion>.*|<PackageVersion>$version</PackageVersion>|" config.props
+
+cd "$(dirname $0)/../server/src"
+sed -i "s|#define MSRV_VERSION_MAJOR.*|#define MSRV_VERSION_MAJOR      $major|" project_info.hpp
+sed -i "s|#define MSRV_VERSION_MINOR.*|#define MSRV_VERSION_MINOR      $minor|" project_info.hpp
