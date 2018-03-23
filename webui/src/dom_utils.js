6e409e14a7650a0f52069c9a54bbf611dcc1f0e9
Hyperblast <hypercode@zoho.com>
2018-03-23 17:08:23 +0300

    inline measure-scrollbar package

--- webui/src/dom_utils.js
+++ webui/src/dom_utils.js
@@ -1,7 +1,23 @@
-import measureScrollBar from 'measure-scrollbar'
 import { once } from './utils'
 
-export const getScrollBarSize = once(measureScrollBar);
+export const getScrollBarSize = once(() =>
+{
+    // Based on https://github.com/sonicdoe/measure-scrollbar/blob/master/index.js
+
+    const div = document.createElement('div');
+
+    div.style.width = '100px';
+    div.style.height = '100px';
+    div.style.overflow = 'scroll';
+    div.style.position = 'absolute';
+    div.style.top = '-9999px';
+
+    document.body.appendChild(div);
+    const size = div.offsetWidth - div.clientWidth;
+    document.body.removeChild(div);
+
+    return size;
+});
 
 let nextElementId = 0;
 
