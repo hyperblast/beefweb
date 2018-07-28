c498f3ae3a02d283c4319b40918890a106eebb8e
Hyperblast <hypercode@zoho.com>
2018-07-28 17:40:15 +0300

    player_foobar2000: move settings to Tools page

--- server/src/player_foobar2000/settings_page.hpp
+++ server/src/player_foobar2000/settings_page.hpp
@@ -18,7 +18,7 @@ public:
 
     virtual const char * get_name() override { return MSRV_PROJECT_NAME; }
     virtual GUID get_guid() override { return guid_; }
-    virtual GUID get_parent_guid() override { return preferences_page::guid_root; }
+    virtual GUID get_parent_guid() override { return preferences_page::guid_tools; }
     virtual bool get_help_url(pfc::string_base & p_out) override { return false;  }
     virtual double get_sort_priority() override { return 1; }
     virtual preferences_page_instance::ptr instantiate(
