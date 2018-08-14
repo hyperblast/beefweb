6610a1378e230bcca746d39e85e3830e455a892b
Hyperblast <hypercode@zoho.com>
2018-08-14 14:42:35 +0300

    reworking travis.yml

--- scripts/install-deadbeef.sh
+++ scripts/install-deadbeef.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-"$(dirname $0)/fetch.sh" \
+cd "$(dirname $0)"
+
+./install.sh \
     deadbeef.x86 \
     'https://sourceforge.net/projects/deadbeef/files/deadbeef-static_0.7.2-2_i686.tar.bz2' \
     cfac56c37d55ccebf934d3b8c0a2b0ae7acf809e9c5aa5dcf6ff0b5a281a4e5b
 
-"$(dirname $0)/fetch.sh" \
+./install.sh \
     deadbeef.x86_64 \
     'https://sourceforge.net/projects/deadbeef/files/deadbeef-static_0.7.2-2_x86_64.tar.bz2' \
     f4ce44690ad80feddfb50b91a75d58b1edbd1cd6491f22d0a67f00debbaea6fc
