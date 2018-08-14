a79de11d0171bd6c092a2afc3d7d73bd3aa59554
Hyperblast <hypercode@zoho.com>
2018-08-14 17:18:46 +0300

    scripts/appveyor/test.cmd: improve diagnostics, run yarn with cmd /c to avoid exiting current script

--- scripts/appveyor/test.cmd
+++ scripts/appveyor/test.cmd
@@ -4,17 +4,26 @@ cd "%~dp0..\.."
 set BEEFWEB_TEST_BUILD_TYPE=release
 set BEEFWEB_USE_BEAST=1
 
+@echo.
+@echo === Running server tests ===
+@echo.
 server\build\release\src\tests\Release\core_tests.exe
-if errorlevel 1 goto :end
+@if errorlevel 1 goto :end
 
-pushd js\api_tests
+@pushd js\api_tests
 
-yarn install
-if errorlevel 1 goto :end
+@echo.
+@echo === Installing packages for API tests ===
+@echo.
+cmd /c yarn install
+@if errorlevel 1 goto :end
 
-yarn test
-if errorlevel 1 goto :end
+@echo.
+@echo === Running API tests ===
+@echo.
+cmd /c yarn test
+@if errorlevel 1 goto :end
 
-popd
+@popd
 
 :end
