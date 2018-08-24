d2cea6eb7dfe4837002fe2d9ef7af14b5c7e5257
Hyperblast <hypercode@zoho.com>
2018-08-24 16:20:16 +0300

    build.proj, upload.proj: support RelWithDebInfo and MinSizeRel targets

--- scripts/appveyor/upload.cmd
+++ scripts/appveyor/upload.cmd
@@ -2,7 +2,7 @@ setlocal
 cd "%~dp0.."
 
 if "%APPVEYOR_REPO_BRANCH%" == "master" (
-    msbuild.cmd upload.proj
+    msbuild.cmd upload.proj /p:Configuration=Release
 ) else (
     @echo Not master branch, skipping upload
 )
