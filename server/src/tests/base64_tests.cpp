295df1f21c2ff07bb4448a40458a69c19c5665bb
Hyperblast <hypercode@zoho.com>
2018-09-11 20:04:29 +0300

    base64: use stringencoders, stringencoders: add missing header

--- server/src/tests/base64_tests.cpp
+++ server/src/tests/base64_tests.cpp
@@ -10,6 +10,7 @@ TEST_CASE("base64")
     REQUIRE(base64Decode("Cg==") == "\n");
     REQUIRE(base64Decode("WU4=") == "YN");
     REQUIRE(base64Decode("SGVsbG8gV29ybGQh") == "Hello World!");
+    REQUIRE(base64Decode("abcd123,") == "");
 }
 
 }}
