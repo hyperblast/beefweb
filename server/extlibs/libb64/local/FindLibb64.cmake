4a6837032d9b11d00d4b82daa23722ff5f046a46
Hyperblast <hypercode@zoho.com>
2018-09-12 10:06:00 +0300

    server: remove libb64 library

--- server/extlibs/libb64/local/FindLibb64.cmake
+++
@@ -1,7 +0,0 @@
-set(LIBB64_INCLUDE_DIRS ${EXTLIB_INSTALL_DIR}/include)
-
-if(MSVC)
-    set(LIBB64_LIBRARIES ${EXTLIB_INSTALL_DIR}/lib/b64.lib)
-else()
-    set(LIBB64_LIBRARIES ${EXTLIB_INSTALL_DIR}/lib/libb64.a)
-endif()
