838322f64706198ecf806a6db9188dbb75c20bed
Hyperblast <hypercode@zoho.com>
2020-02-09 15:39:31 +0300

    appveyor/build.cmd: switch to VS2017 with dynamic runtime

--- scripts/appveyor/build.cmd
+++ scripts/appveyor/build.cmd
@@ -3,4 +3,4 @@ cd "%~dp0.."
 
 msbuild.cmd build.proj ^
     /p:Configuration=%BUILD_TYPE% /p:EnableTests=True ^
-    /p:ServerBuildFlags="-DENABLE_STATIC_STDLIB=ON;-DENABLE_GIT_REV=ON"
+    /p:ServerBuildFlags="-DENABLE_STATIC_STDLIB=OFF;-DENABLE_GIT_REV=ON"
