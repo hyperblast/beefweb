58c8c9cca144338f84c63f6ee169366f9739bf8e
Hyperblast <hypercode@zoho.com>
2020-02-09 12:29:51 +0300

    remove x86 support from ci scripts
    it is still possible to build x86 manually

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -4,8 +4,5 @@ set -e
 
 cd "$(dirname $0)/../install"
 
-./deadbeef.sh v0.7 "$TARGET_ARCH"
-
-if [ "$TARGET_ARCH" = "x86_64" ]; then
-    ./deadbeef.sh v1.8 "$TARGET_ARCH"
-fi
+./deadbeef.sh v0.7
+./deadbeef.sh v1.8
