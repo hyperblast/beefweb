3f2f979c7bed11095752db3b189d537c61987d79
Hyperblast <dev@bitflow.org>
2022-06-04 09:10:59 +0300

    remove old CI stuff

--- ci/appveyor/build.cmd
+++
@@ -1,6 +0,0 @@
-setlocal
-cd "%~dp0..\.."
-
-cmd /c scripts\msbuild.cmd scripts\build.proj ^
-    /p:Configuration=%BUILD_TYPE% /p:EnableTests=True ^
-    /p:ServerBuildFlags="-DENABLE_STATIC_STDLIB=OFF;-DENABLE_GIT_REV=ON"
--- ci/travis/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd "$(dirname $0)/../.."
-
-scripts/install/deadbeef.sh v0.7
-scripts/install/deadbeef.sh v1.8
