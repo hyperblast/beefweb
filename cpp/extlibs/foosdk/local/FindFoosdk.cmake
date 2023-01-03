b6185f3ae8a989b71f20edd85e695a67ce04e210
Hyperblast <dev@bitflow.org>
2023-01-03 08:33:53 +0300

    use latest foosdk

--- cpp/extlibs/foosdk/local/FindFoosdk.cmake
+++ cpp/extlibs/foosdk/local/FindFoosdk.cmake
@@ -1,7 +1,15 @@
 set(FOOSDK_INCLUDE_DIRS ${EXTLIB_INSTALL_DIR}/include)
 
+if(${CMAKE_SIZEOF_VOID_P} STREQUAL 4)
+    set(FOOSDK_ARCH_SUFFIX "Win32")
+elseif(${CMAKE_SIZEOF_VOID_P} STREQUAL 8)
+    set(FOOSDK_ARCH_SUFFIX "x64")
+else()
+    message(SEND_ERROR "Unknown pointer size")
+endif()
+
 set(
     FOOSDK_LIBRARIES
     ${EXTLIB_INSTALL_DIR}/lib/foosdk.lib
-    ${EXTLIB_INSTALL_DIR}/lib/shared.lib
+    ${EXTLIB_INSTALL_DIR}/lib/shared-${FOOSDK_ARCH_SUFFIX}.lib
 )
