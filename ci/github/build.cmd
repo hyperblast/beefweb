5e6341a49333832a645d34d29c5be662441e5171
Hyperblast <dev@bitflow.org>
2023-01-03 12:45:11 +0300

    build and test in 32 and 64 modes

--- ci/github/build.cmd
+++ ci/github/build.cmd
@@ -2,5 +2,5 @@ setlocal
 cd "%~dp0..\.."
 
 msbuild.exe scripts\build.proj ^
-    /p:Configuration=%BUILD_TYPE% /p:EnableTests=True ^
+    /p:Platform=%BUILD_ARCH% /p:Configuration=%BUILD_TYPE% /p:EnableTests=True ^
     /p:ServerBuildFlags="-DENABLE_STATIC_STDLIB=OFF;-DENABLE_GIT_REV=ON"
--- ci/github/install.cmd
+++ ci/github/install.cmd
@@ -4,7 +4,7 @@ cd "%~dp0..\.."
 cmd /c scripts\install\patch.cmd
 @if errorlevel 1 goto :end
 
-if "%Platform%" == "x64" (
+if "%BUILD_ARCH%" == "x64" (
     cmd /c scripts\install\foobar2000.cmd v2.0-x64
     @if errorlevel 1 goto :end
 ) else (
