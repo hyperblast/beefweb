9a0819b6b61f231768ba8918587c22d73f7ddefe
Hyperblast <dev@bitflow.org>
2024-05-25 13:42:13 +0500

    server/deadbeef/artwork_fetcher_v1: code style tweaks

--- cpp/server/deadbeef/artwork_fetcher_v1.cpp
+++ cpp/server/deadbeef/artwork_fetcher_v1.cpp
@@ -8,14 +8,13 @@
 namespace msrv {
 namespace player_deadbeef {
 
+namespace {
+
 class ArtworkRequestV1
     : public boost::intrusive_ref_counter<ArtworkRequestV1, boost::thread_safe_counter>
 {
 public:
-    static boost::intrusive_ptr<ArtworkRequestV1> create()
-    {
-        return {new ArtworkRequestV1()};
-    }
+    static boost::intrusive_ptr<ArtworkRequestV1> create() { return {new ArtworkRequestV1()}; }
 
     ~ArtworkRequestV1() = default;
 
@@ -30,7 +29,7 @@ private:
     void complete(const char* filePath, const char* artist, const char* album);
 
     std::string resultPath_;
-    boost::promise<ArtworkResult> promise_;
+    boost::promise<ArtworkResult> resultPromise_;
 
     MSRV_NO_COPY_AND_ASSIGN(ArtworkRequestV1);
 };
@@ -52,10 +51,10 @@ boost::unique_future<ArtworkResult> ArtworkRequestV1::execute(
     {
         intrusive_ptr_release(this);
         logDebug("artwork found in cache: %s", cachedResultPath.get());
-        promise_.set_value(ArtworkResult(std::string(cachedResultPath.get())));
+        resultPromise_.set_value(ArtworkResult(std::string(cachedResultPath.get())));
     }
 
-    return promise_.get_future();
+    return resultPromise_.get_future();
 }
 
 void ArtworkRequestV1::complete(const char* filePath, const char* artist, const char* album)
@@ -68,12 +67,12 @@ void ArtworkRequestV1::complete(const char* filePath, const char* artist, const 
             artist ? artist : "",
             album ? album : "");
 
-        promise_.set_value(ArtworkResult(resultPath_));
+        resultPromise_.set_value(ArtworkResult(resultPath_));
     }
     else
     {
         logDebug("artwork is not found");
-        promise_.set_value(ArtworkResult());
+        resultPromise_.set_value(ArtworkResult());
     }
 }
 
@@ -83,6 +82,8 @@ void ArtworkRequestV1::callback(
     boost::intrusive_ptr<ArtworkRequestV1> request(reinterpret_cast<ArtworkRequestV1*>(data), false);
 
     tryCatchLog([&] { request->complete(filePath, artist, album); });
+}
+
 }
 
 class ArtworkFetcherV1 : public ArtworkFetcher
@@ -100,6 +101,17 @@ private:
     std::vector<TitleFormatPtr> columns_;
 };
 
+boost::unique_future<ArtworkResult> ArtworkFetcherV1::fetchArtwork(PlaylistPtr playlist, PlaylistItemPtr item)
+{
+    auto columns = evaluateColumns(playlist.get(), item.get(), columns_);
+    auto path = pathFromUtf8(columns[0]);
+    auto pathString = path.empty() ? nullptr : path.c_str();
+    auto artist = columns[1].empty() ? nullptr : columns[1].c_str();
+    auto album = columns[2].empty() ? nullptr : columns[2].c_str();
+    auto request = ArtworkRequestV1::create();
+    return request->execute(plugin_, pathString, artist, album);
+}
+
 std::unique_ptr<ArtworkFetcher> ArtworkFetcher::createV1()
 {
     auto artwork = ddbApi->plug_get_for_id("artwork");
@@ -113,17 +125,6 @@ std::unique_ptr<ArtworkFetcher> ArtworkFetcher::createV1()
     return std::make_unique<ArtworkFetcherV1>(
         reinterpret_cast<DB_artwork_plugin_t*>(artwork),
         std::move(columns));
-}
-
-boost::unique_future<ArtworkResult> ArtworkFetcherV1::fetchArtwork(PlaylistPtr playlist, PlaylistItemPtr item)
-{
-    auto columns = evaluateColumns(playlist.get(), item.get(), columns_);
-    auto path = pathFromUtf8(columns[0]);
-    auto pathString = path.empty() ? nullptr : path.c_str();
-    auto artist = columns[1].empty() ? nullptr : columns[1].c_str();
-    auto album = columns[2].empty() ? nullptr : columns[2].c_str();
-    auto request = ArtworkRequestV1::create();
-    return request->execute(plugin_, pathString, artist, album);
 }
 
 }}
