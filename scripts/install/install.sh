75213e4294b9ee56e7c5f6c36a477f56d0fd68b4
Hyperblast <dev@bitflow.org>
2024-06-02 10:06:31 +0500

    scripts: allow to install apps globally

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -7,18 +7,23 @@ url="$2"
 hash="$3"
 
 if [ -z "$name" ] || [ -z "$url" ] || [ -z "$hash" ]; then
-    echo "usage: $(basename $0) <name> <url> <hash>"
+    echo "usage: $(basename $0) <name>|global <url> <hash>"
     exit 1
 fi
 
-cd "$(dirname $0)/../.."
-
 file="${url##*/}"
-install_dir="apps/$name"
 
-rm -rf "$install_dir"
-mkdir -p "$install_dir"
-cd "$install_dir"
+if [ "$name" = "global" ]; then
+    cd /usr/local
+else
+    cd "$(dirname $0)/../.."
+
+    install_dir="apps/$name"
+
+    rm -rf "$install_dir"
+    mkdir -p "$install_dir"
+    cd "$install_dir"
+fi
 
 curl --silent --fail --show-error --location -o "$file" "$url"
 echo "$hash *$file" | sha256sum -c
--- scripts/install/node.sh
+++ scripts/install/node.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-"$(dirname $0)/install.sh" node \
+"$(dirname $0)/install.sh" global \
     'https://nodejs.org/dist/v12.22.12/node-v12.22.12-linux-x64.tar.xz' \
     'e6d052364bfa2c17da92cf31794100cfd709ba147415ddaeed2222eec9ca1469'
 
-NODE_BIN=$(readlink -f "$(dirname $0)/../../apps/node/bin")
-
-export PATH="$NODE_BIN:$PATH"
+export PATH="/usr/local/bin:$PATH"
 
 npm i -g yarn@1.22.22
