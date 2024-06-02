4c36f91e8ea48e5bf7ec8331cd3afa37b9e07a12
Hyperblast <dev@bitflow.org>
2024-06-02 09:17:51 +0500

    fix yarn installation

--- scripts/install/node.sh
+++ scripts/install/node.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-
 "$(dirname $0)/install.sh" node \
     'https://nodejs.org/dist/v12.22.12/node-v12.22.12-linux-x64.tar.xz' \
     'e6d052364bfa2c17da92cf31794100cfd709ba147415ddaeed2222eec9ca1469'
 
-cd "$(dirname $0)/../../apps/node/lib"
-../bin/npm i yarn@1.22.22 --no-save
-ln -s ../lib/node_modules/yarn/bin/yarn.js ../bin/yarn
+NODE_BIN=$(readlink -f "$(dirname $0)/../../apps/node/bin")
+
+export PATH="$NODE_BIN:$PATH"
+
+npm i -g yarn@1.22.22
