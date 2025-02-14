5c37f7668e4b56cea975cdafe8a0b20818c1d54a
Hyperblast <dev@bitflow.org>
2025-02-14 17:27:02 +0500

    fix ENABLE_GIT_REV not being propagated

--- ci/build.cmd
+++ ci/build.cmd
@@ -3,4 +3,4 @@ cd "%~dp0.."
 
 msbuild.exe scripts\build.proj ^
     /p:Platform=%BUILD_ARCH% /p:Configuration=%BUILD_TYPE% /p:EnableTests=True ^
-    /p:ServerBuildFlags="-DENABLE_STATIC_STDLIB=OFF;-DENABLE_GIT_REV=ON"
+    /p:BuildFlags="-DENABLE_GIT_REV=ON"
