c58e13c79a51931608634f02149be7a844db5ea0
Hyperblast <dev@bitflow.org>
2025-02-28 17:00:46 +0500

    ci/build.cmd: build without helper script

--- ci/build.cmd
+++ ci/build.cmd
@@ -1,6 +1,25 @@
-setlocal
-cd "%~dp0.."
+@setlocal
+@cd "%~dp0.."
 
-msbuild.exe scripts\build.proj ^
-    /p:Platform=%BUILD_ARCH% /p:Configuration=%BUILD_TYPE% /p:EnableTests=True ^
-    /p:BuildFlags="-DENABLE_GIT_REV=ON"
+@if [%BUILD_TYPE%] == [] (
+    @echo BUILD_TYPE is not set, aborting
+    @cmd /c "exit 1"
+    goto :end
+)
+
+rmdir /S /Q ci_build\%BUILD_TYPE%
+mkdir ci_build\%BUILD_TYPE%
+cd ci_build\%BUILD_TYPE%
+
+cmake ../.. -DENABLE_TESTS=ON -DENABLE_GIT_REV=ON
+@if errorlevel 1 goto :end
+
+cmake --build . --config %BUILD_TYPE%
+@if errorlevel 1 goto :end
+
+cpack -C %BUILD_TYPE%
+@if errorlevel 1 goto :end
+
+ren *.zip *.fb2k-component
+
+:end
