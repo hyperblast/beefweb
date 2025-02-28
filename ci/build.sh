99cc70189a96ac503f2793121712cc9538c6647b
Hyperblast <dev@bitflow.org>
2025-02-28 16:42:57 +0500

    ci: build without helper script

--- ci/build.sh
+++ ci/build.sh
@@ -4,8 +4,20 @@ set -e
 
 function main
 {
-    scripts/build.sh --all --$BUILD_TYPE --tests --verbose \
-        -DENABLE_WERROR=ON -DENABLE_STATIC_STDLIB=ON -DENABLE_GIT_REV=ON
+    rm -rf ci_build/$BUILD_TYPE
+    mkdir -p ci_build/$BUILD_TYPE
+    cd ci_build/$BUILD_TYPE
+
+    cmake ../.. \
+        -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+        -DENABLE_TESTS=ON \
+        -DENABLE_WERROR=ON \
+        -DENABLE_STATIC_STDLIB=ON \
+        -DENABLE_GIT_REV=ON
+
+    cmake --build . --parallel
+
+    cpack
 }
 
 source "$(dirname $0)/run-in-docker.sh"
