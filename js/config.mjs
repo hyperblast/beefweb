5f03fc54260b5db97d4c4238992dd7ec36509679
Hyperblast <dev@bitflow.org>
2025-03-01 08:52:47 +0500

    remove old code

--- js/config.mjs
+++ js/config.mjs
@@ -5,20 +5,6 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-function readFirstLine(filePath)
-{
-    try
-    {
-        const data = fs.readFileSync(filePath, { encoding: 'utf-8' });
-        const match = data.match(/.+/);
-        return match ? match[0] : null;
-    }
-    catch (err)
-    {
-        return null;
-    }
-}
-
 function toAbsolutePath(p)
 {
     return path.isAbsolute(p) ? p : path.join(path.dirname(__dirname), p);
