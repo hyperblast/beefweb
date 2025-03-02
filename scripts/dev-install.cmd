f6b4c7094df8eec1f28469e241bd8deef5287113
Hyperblast <dev@bitflow.org>
2025-03-02 11:13:58 +0500

    dev-install.cmd: make syntax closer to dev-install.sh

--- scripts/dev-install.cmd
+++ scripts/dev-install.cmd
@@ -2,16 +2,24 @@
 
 @setlocal
 
+@if [%1] == [] (
+    @echo Usage: %~nx0 ^<build_type^>
+    @goto :end
+)
+
+set BUILD_TYPE=%1
 set PROFILE_DIR=%APPDATA%\foobar2000-v2
 set COMPONENT_DIR=%PROFILE_DIR%\user-components-x64\foo_beefweb
 set CONFIG_DIR=%PROFILE_DIR%\beefweb
-
-@if [%BUILD_TYPE%] == [] set BUILD_TYPE=Debug
 
 mkdir "%COMPONENT_DIR%"
 mkdir "%CONFIG_DIR%"
 
 cd "%~dp0..\build\%BUILD_TYPE%\cpp\server"
 
+@if errorlevel 1 goto :end
+
 copy /Y foobar2000\%BUILD_TYPE%\*.* "%COMPONENT_DIR%"
 copy /Y %BUILD_TYPE%\config.json    "%CONFIG_DIR%"
+
+:end
