1fece07ff1b075fe24cc2c7840cfbf33a3227128
Hyperblast <dev@bitflow.org>
2025-03-03 06:29:44 +0500

    fixes

--- cmake/CPackCustomConfig.cmake
+++ cmake/CPackCustomConfig.cmake
@@ -2,8 +2,6 @@ if(${CPACK_GENERATOR} STREQUAL "TXZ")
     if(NOT CPACK_DEADBEEF_SINGLE_DIR)
         message(FATAL_ERROR "ENABLE_DEADBEEF_SINGLE_DIR should be ON when building .tar.xz package")
     endif()
-
-    set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/deadbeef")
 endif()
 
 if(${CPACK_GENERATOR} STREQUAL "DEB")
