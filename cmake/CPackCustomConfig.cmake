0a7c814bade299e2944e7d3157cc6537d720acf5
Hyperblast <dev@bitflow.org>
2025-03-03 06:14:13 +0500

    better option validation

--- cmake/CPackCustomConfig.cmake
+++ cmake/CPackCustomConfig.cmake
@@ -1,3 +1,15 @@
+if(${CPACK_GENERATOR} STREQUAL "TXZ")
+    if(NOT CPACK_DEADBEEF_SINGLE_DIR)
+        message(FATAL_ERROR "ENABLE_DEADBEEF_SINGLE_DIR should be ON when building .tar.xz package")
+    endif()
+
+    set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/deadbeef")
+endif()
+
 if(${CPACK_GENERATOR} STREQUAL "DEB")
+    if(CPACK_DEADBEEF_SINGLE_DIR)
+        message(FATAL_ERROR "ENABLE_DEADBEEF_SINGLE_DIR should be OFF when building .deb package")
+    endif()
+
     set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/deadbeef")
 endif()
