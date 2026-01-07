f4a69b5f61d38cf181885561e47a41886d7e5b6a
Hyperblast <dev@bitflow.org>
2026-01-07 16:06:47 +0500

    window_controller: use emoji for playback state

--- js/webui/src/window_controller.js
+++ js/webui/src/window_controller.js
@@ -1,36 +1,9 @@
-import iconPlay from 'open-iconic/png/media-play-4x.png'
-import iconPause from 'open-iconic/png/media-pause-4x.png'
-import iconStop from 'open-iconic/png/media-stop-4x.png'
 import { PlaybackState } from 'beefweb-client'
 
 const stateToIcon = {
-    [PlaybackState.playing]: iconPlay,
-    [PlaybackState.paused]: iconPause,
-    [PlaybackState.stopped]: iconStop
+    [PlaybackState.playing]: '\u25B6\uFE0F',
+    [PlaybackState.paused]: '\u23F8\uFE0F'
 };
-
-function setIcon(icon)
-{
-    let iconElement = document.getElementById('player-state-icon');
-
-    if (iconElement)
-    {
-        iconElement.href = icon;
-        return;
-    }
-
-    iconElement =  document.createElement('link');
-    iconElement.id = 'player-state-icon';
-    iconElement.rel = 'shortcut icon';
-    iconElement.href = icon;
-
-    document.head.appendChild(iconElement);
-}
-
-function stateEqual(x, y)
-{
-    return x.title === y.title && x.playbackState === y.playbackState;
-}
 
 export default class WindowController
 {
@@ -43,37 +16,17 @@ export default class WindowController
     start()
     {
         this.playerModel.on('change', this.handleUpdate);
-
-        this.state = {
-            title: '',
-            playbackState: ''
-        };
-
         this.handleUpdate();
-    }
-
-    getStateFromModel()
-    {
-        const model = this.playerModel;
-        const playbackState = model.playbackState;
-
-        const title = playbackState !== PlaybackState.stopped
-            ? model.activeItem.columns[0] + ' - ' + model.info.title
-            : model.info.title;
-
-        return { title, playbackState };
     }
 
     handleUpdate()
     {
-        const state = this.getStateFromModel();
+        const model = this.playerModel;
+        const state = model.playbackState;
 
-        if (stateEqual(this.state, state))
-            return;
-
-        this.state = state;
-
-        window.document.title = state.title;
-        setIcon(stateToIcon[state.playbackState]);
+        window.document.title =
+            state === PlaybackState.stopped
+                ? model.info.title
+                : stateToIcon[state] + ' ' + model.activeItem.columns[0] + ' - ' + model.info.title;
     }
 }
