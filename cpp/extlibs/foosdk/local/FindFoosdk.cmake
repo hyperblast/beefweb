e2d4db6e352bd8f2820d1f1332b6910279d77ac0
Hyperblast <dev@bitflow.org>
2026-01-15 23:07:16 +0500

    linking fixes

--- cpp/extlibs/foosdk/local/FindFoosdk.cmake
+++ cpp/extlibs/foosdk/local/FindFoosdk.cmake
@@ -1,16 +1,23 @@
 set(FOOSDK_INCLUDE_DIRS ${EXTLIB_INSTALL_DIR}/include)
 
-set(
-    FOOSDK_LIBRARIES
-    ${EXTLIB_INSTALL_DIR}/lib/foosdk.lib
-)
-
 if(OS_WINDOWS)
-    set_by_pointer_size(
+	set(
+		FOOSDK_LIBRARIES
+		${EXTLIB_INSTALL_DIR}/lib/foosdk.lib
+	)
+
+	set_by_pointer_size(
         FOOSDK_SHARED_LIB
         shared-Win32.lib
         shared-x64.lib
     )
 
     list(APPEND FOOSDK_LIBRARIES ${EXTLIB_INSTALL_DIR}/lib/${FOOSDK_SHARED_LIB})
+endif()
+
+if(OS_MAC)
+    set(
+        FOOSDK_LIBRARIES
+        ${EXTLIB_INSTALL_DIR}/lib/libfoosdk.a
+	)
 endif()
