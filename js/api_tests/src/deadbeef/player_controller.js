fc564dbad5d506448d8b10742b5e8b44cfeb8881
Hyperblast <dev@bitflow.org>
2026-01-17 12:26:14 +0500

    debug code

--- js/api_tests/src/deadbeef/player_controller.js
+++ js/api_tests/src/deadbeef/player_controller.js
@@ -121,8 +121,9 @@ class PlayerController
 
                     async mac()
                     {
+                        console.error('killall begin');
                         const result = await execFile('killall', ['DeaDBeeF']);
-                        console.error('killall:', result);
+                        console.error('killall end:', result);
                         await waitForExit(this.process);
                     },
                 });
