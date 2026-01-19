14e6d7d2f95b2e4e4c83b04f1987692e6b6082ea
Hyperblast <dev@bitflow.org>
2026-01-19 19:20:49 +0500

    trying tests again

--- ci/macos_test.sh
+++ ci/macos_test.sh
@@ -17,18 +17,34 @@ function run_server_tests
     ci_build/$BUILD_TYPE/cpp/server/tests/core_tests
 }
 
+function run_foobar2000_tests
+{
+    banner "Running API tests on foobar2000 $1"
+
+    export BEEFWEB_TEST_PLAYER=foobar2000
+    export BEEFWEB_TEST_FOOBAR2000_VERSION=$1
+
+    (cd js/api_tests && yarn test)
+}
+
 function run_deadbeef_tests
 {
     banner "Running API tests on deadbeef $1"
 
-    export BEEFWEB_BINARY_DIR_BASE=ci_build
+    export BEEFWEB_TEST_PLAYER=deadbeef
     export BEEFWEB_TEST_DEADBEEF_VERSION=$1
-    export BEEFWEB_TEST_BUILD_TYPE=$BUILD_TYPE
 
     (cd js/api_tests && yarn test)
 }
 
 run_server_tests
+
+export BEEFWEB_BINARY_DIR_BASE=ci_build
+export BEEFWEB_TEST_BUILD_TYPE=$BUILD_TYPE
+
+for version in $(scripts/install_app.sh list-versions foobar2000); do
+    run_foobar2000_tests $version
+done
 
 for version in $(scripts/install_app.sh list-versions deadbeef); do
     run_deadbeef_tests $version
