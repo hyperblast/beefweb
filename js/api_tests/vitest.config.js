1fa337a50d644654dfb45dbf3408186199decd2c
Hyperblast <dev@bitflow.org>
2026-01-21 07:37:35 +0500

    api_tests: disable test isolation

--- js/api_tests/vitest.config.js
+++ js/api_tests/vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         include: ['./src/*_tests.js'],
+        isolate: false,
         bail: 10,
         retry: 1,
         maxConcurrency: 1,
