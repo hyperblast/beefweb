58c4e4451eae695a14f66091abdb5c31923889c6
Hyperblast <dev@bitflow.org>
2026-01-24 08:47:32 +0500

    macos: add settings page stub

---
+++ cpp/server/foobar2000/macos/main_prefs_page.mm
@@ -0,0 +1,74 @@
+#import <Cocoa/Cocoa.h>
+
+#include "plugin_settings.hpp"
+
+using namespace msrv::player_foobar2000;
+
+@interface MainPrefsPageInstance : NSViewController
+@end
+
+@interface MainPrefsPageInstance : ()
+@end
+
+@implementation MainPrefsPageInstance
+
+- (void)setupView
+{
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setupView];
+}
+
+- (void)viewWillAppear
+{
+    [super viewWillAppear];
+}
+
+
+@end
+
+namespace {
+    
+class MainPrefsPage : public preferences_page_v3
+{
+public:
+    MainPrefsPage() = default;
+    ~MainPrefsPage() = default;
+
+    const char* get_name() override
+    {
+        return MSRV_PROJECT_NAME;
+    }
+
+    GUID get_guid() override
+    {
+        return prefs_pages::main;
+    }
+
+    GUID get_parent_guid() override
+    {
+        return preferences_page::guid_tools;
+    }
+
+    bool get_help_url(pfc::string_base& p_out) override
+    {
+        return false;
+    }
+
+    double get_sort_priority() override
+    {
+        return 0;
+    }
+
+    service_ptr instantiate() override
+    {
+        return fb2k::wrapNSObject([MainPrefsPageInstance new]);
+    }
+};
+
+preferences_page_factory_t<MainPrefsPage> factory;
+
+}
