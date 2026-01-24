5331d9284313faf8ac5625ada4885e839703be18
Hyperblast <dev@bitflow.org>
2026-01-24 14:00:25 +0300

    prefs page wip

--- cpp/server/foobar2000/macos/main_prefs_page.mm
+++ cpp/server/foobar2000/macos/main_prefs_page.mm
@@ -9,27 +9,95 @@ using namespace msrv::player_foobar2000;
 @end
 
 @interface MainPrefsPageInstance ()
+    @property(strong, nonatomic) NSTextField* portText;
+    @property(strong, nonatomic) NSButton* allowRemoteButton;
+    @property(strong, nonatomic) NSTextField* musicDirsText;
+    @property(strong, nonatomic) NSButton* authRequiredButton;
+    @property(strong, nonatomic) NSTextField* authUserText;
+    @property(strong, nonatomic) NSSecureTextField* authPasswordText;
 @end
 
 @implementation MainPrefsPageInstance
 
+- (NSBox*)boxWithTitle:(NSString*)title views:(NSArray<NSView*>*)views
+{
+    NSBox* box = [[NSBox alloc] init];
+    box.boxType = NSBoxPrimary;
+    box.title = title;
+
+    NSStackView* stack = [NSStackView stackViewWithViews:views];
+    stack.alignment = NSLayoutAttributeLeading;
+    stack.orientation = NSUserInterfaceLayoutOrientationVertical;
+    [box setContentView:stack];
+    return box;
+}
+
+- (IBAction)allowRemoteChanged:(id)sender
+{
+}
+
+- (IBAction)requireAuthChanged:(id)sender
+{
+    BOOL enabled = [self.authRequiredButton state] == NSControlStateValueOn;
+    self.authUserText.editable = enabled;
+    self.authPasswordText.editable = enabled;
+}
+
 - (void)setupView
 {
-    CGFloat margin = 20;
-
-    NSStackView* rootStack = [NSStackView stackViewWithViews:@[
-        [NSTextField labelWithString:@"Hello"]
-    ]];
-
-    rootStack.alignment = NSLayoutAttributeLeading;
-    rootStack.orientation = NSUserInterfaceLayoutOrientationVertical;
-    [self.view addSubview:rootStack];
+    self.portText = [NSTextField textFieldWithString:@""];
 
     [NSLayoutConstraint activateConstraints:@[
-        [rootStack.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:margin],
-        [rootStack.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:margin],
-        [rootStack.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-margin],
-        [rootStack.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-margin],
+        [self.portText.widthAnchor constraintEqualToConstant:50],
+        [self.portText.heightAnchor constraintEqualToConstant:25],
+    ]];
+
+    self.allowRemoteButton = [
+        NSButton checkboxWithTitle:@"Allow remote connections"
+                 target:self
+                 action:@selector(allowRemoteChanged:)
+    ];
+
+    self.musicDirsText = [NSTextField textFieldWithString:@""];
+    self.musicDirsText.usesSingleLineMode = NO;
+
+    [NSLayoutConstraint activateConstraints:@[
+        [self.musicDirsText.heightAnchor constraintEqualToConstant:200]
+    ]];
+
+    self.authRequiredButton = [
+        NSButton checkboxWithTitle:@"Require authentication"
+                 target:self
+                 action:@selector(requireAuthChanged:)
+    ];
+
+    self.authUserText = [NSTextField textFieldWithString:@""];
+    self.authPasswordText = [[NSSecureTextField alloc] init];
+
+    NSStackView* stack = [NSStackView stackViewWithViews:@[
+        [NSTextField labelWithString: @"Port:"],
+        self.portText,
+        self.allowRemoteButton,
+        [NSTextField labelWithString: @"Browseable music directories:"],
+        self.musicDirsText,
+        self.authRequiredButton,
+        [NSTextField labelWithString: @"User:"],
+        self.authUserText,
+        [NSTextField labelWithString: @"Password:"],
+        self.authPasswordText,
+    ]];
+
+    stack.alignment = NSLayoutAttributeLeading;
+    stack.orientation = NSUserInterfaceLayoutOrientationVertical;
+    [self.view addSubview:stack];
+
+    CGFloat margin = 20;
+
+    [NSLayoutConstraint activateConstraints:@[
+        [stack.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:margin],
+        [stack.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:margin],
+        [stack.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-margin],
+        [stack.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-margin],
     ]];
 }
 
